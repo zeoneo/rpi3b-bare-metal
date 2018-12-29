64d8ae159c3b8eee2d99b6be57d082e51201c15b
zeo <zeo29021992@gmail.com>
2018-12-29 22:31:31 +0530

    Add debug script and qemu script

---
+++ rpi3b-meaty-skeleton/debug.sh
@@ -0,0 +1,1 @@
+arm-none-eabi-gdb -ex "target remote localhost:1234" -ex "symbol-file kernel/kernel8-32.elf"
---
+++ rpi3b-meaty-skeleton/qemu.sh
@@ -0,0 +1,1 @@
+qemu-system-aarch64 -s -S -m 1024M -M raspi2 -serial stdio -kernel kernel/kernel8-32.elf
